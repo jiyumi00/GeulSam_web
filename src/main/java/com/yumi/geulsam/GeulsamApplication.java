4e48cd375124e7701c17192784fbe657a4128ce8
Jiyun <jiyumi00@naver.com>
2024-09-28 01:32:00 +0900

    init project

---
+++ src/main/java/com/yumi/geulsam/GeulsamApplication.java
@@ -0,0 +1,13 @@
+package com.yumi.geulsam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeulsamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeulsamApplication.class, args);
+	}
+
+}
